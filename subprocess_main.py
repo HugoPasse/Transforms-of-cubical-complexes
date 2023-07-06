b998ba17ea1fd5e2c72e587437d5a0bdd005d869
vadimlebovici <49746979+vadimlebovici@users.noreply.github.com>
2023-07-06 11:39:48 +0200

    expé ready ect

--- subprocess_main.py
+++ subprocess_main.py
@@ -1,12 +1,52 @@
+#%%
 import numpy as np
 import os
 import subprocess
 
-spacings = [1*(i+1) for i in range(10)]
-n = 1000
+sizes = [10, 20, 30]
+n_dir = 10
+spacings = [1*(i+1) for i in range(3)]
+n_samples = 2
 dual = 0
+title = 'test'
+transform = 'ECT' # 'ECT' or 'Radon' or 'HT'
+overwrite_lock = str(np.random.rand())
+path_to_savings = 'timings/' + title + '-' + overwrite_lock
 
-for s in spacings:
-	print('n = ',n,'| spacing =',s)
-	cmd = 'python3 subprocess_mem.py ' +  str(n) + ' ' + str(s) + ' ' + str(dual)
-	os.system(cmd)
+# TODO: critical_points, critical_values, record_all
+
+print('######################')
+print('transform:', transform)
+print('sizes:', sizes)
+print('spacings:\n', spacings)
+print('nbre directions:', n_dir)
+print('dual:', dual)
+
+with open(path_to_savings + '-logs.txt', 'a+') as file:
+	file.write('############\n\n')
+	file.write(f'transform:{transform}\n')
+	file.write(f'sizes: {sizes}\n')
+	file.write(f'spacings:\n{spacings}\n')
+	file.write(f'nbre directions: {n_dir}\n')
+	file.write(f'dual: {dual}\n')
+	file.write('(time (s),memory (KB)): \n init cplx \n upper_star \n pre_processing \n transform \n total \n\n')
+	file.close()
+
+result = np.zeros((n_samples,len(sizes),len(spacings),5,2))
+n_crit_pts = np.zeros((n_samples,len(sizes),len(spacings)))
+for _ in range(n_samples):
+	for i, size in enumerate(sizes):
+		for j, spacing in enumerate(spacings):
+			print(f'sample = {_} | size = {size} | spacing = {spacing}')
+			cmd = 'python3 subprocess_mem.py ' +  str(size) + ' ' + str(spacing) + ' ' + str(dual) + ' ' + path_to_savings
+			output = subprocess.check_output(cmd, shell=True)
+			temp_res = [float(_.decode()) for _ in output.split()]
+			result[_,i,j] = np.array([(temp_res[2*_],temp_res[2*_+1]) for _ in range(4)]+[(sum(temp_res[2*_] for _ in range(4)), sum(temp_res[2*_+1] for _ in range(4)))])
+			n_crit_pts[_,i,j] = int(temp_res[-1])
+			with open(path_to_savings+'-logs.txt', 'a+') as file:
+				file.write(f'\n nbr critical points: {n_crit_pts[_,i,j]}\n')
+				file.write(f'result:\n{result[_,i,j]}\n')
+				file.close()
+np.savez(path_to_savings, result=result, n_crit_pts=n_crit_pts)
+print('Results saved.')
+# %%
--- subprocess_mem.py
+++ subprocess_mem.py
@@ -2,19 +2,25 @@ import numpy as np
 import embedded_cubical_complex as ecc
 from test_shapes import regular_points
 import sys
+import time
 import os, psutil
 process = psutil.Process()
 
-if len(sys.argv) != 4:
+if len(sys.argv) != 5:
 	print('Incorrect arguments')
 	exit()
 
 n = sys.argv[1]
 spacing = sys.argv[2]
 dual = sys.argv[3]
+path_to_savings = sys.argv[4]
 
 if not (n.isdigit() and spacing.isdigit() and dual.isdigit()):
 	print("Arguments must be integers")
+	exit()
+
+if not path_to_savings.isprintable():
+	print("path_to_savings must be printable")
 	exit()
 
 n = int(n)
@@ -24,13 +30,44 @@ if not dual == 0 : dual = 1
 
 img = np.array(regular_points((n,n),np.array([spacing,spacing]),np.array([spacing,spacing])))
 
-mem1 = process.memory_info().rss
-
+# Construct complex
+tic = time.perf_counter()
+mtic = process.memory_info().rss
 cplx = ecc.EmbeddedComplex(img*(-1)**dual, 1, input_top_cells=(not dual))
+mtoc = process.memory_info().rss
+toc = time.perf_counter()
+time_cplx = toc-tic
+print(f'{time_cplx} {(mtoc-mtic)/1000}', end=' ')
+
+# Impose upper star
+time_ustar = 0
 if not dual:
+	tic = time.perf_counter()
 	cplx.impose_upper_star_filtration()
+	toc = time.perf_counter()
+	time_ustar = toc-tic
+print(f'{time_ustar} 0', end=' ')
+
+# Preprocessing
+mtic = process.memory_info().rss
+tic = time.perf_counter()
 cplx.init_ect(1)
+toc = time.perf_counter()
+mtoc = process.memory_info().rss
+time_preproc = toc-tic
+print(f'{time_preproc} {(mtoc-mtic)/1000}', end=' ')
 
-mem2 = process.memory_info().rss
+# Computing transform
+transform = 0
+mtic = process.memory_info().rss
+tic = time.perf_counter()
+for i in range(10):
+	transform = cplx.compute_euler_caracteristic_transform(np.random.rand(2))
+toc = time.perf_counter()
+mtoc = process.memory_info().rss
+time_transform = toc-tic
+print(f'{time_transform} {(mtoc-mtic)/1000}', end=' ')
 
-print((mem2 - mem1) / (1024**2))
+
+cplx.init_hybrid_transform(1)
+print(f'{len(cplx.get_critical_vertices(0))}')
