ccb95b3a88a206cc2357e7fff1daf70b07fa0d63
vadimlebovici <49746979+vadimlebovici@users.noreply.github.com>
2023-07-08 15:56:23 +0200

    correct expe

--- subprocess_main.py
+++ subprocess_main.py
@@ -54,26 +54,27 @@ def main(expe=0, transform='HT'): # transform = 'ECT' or 'Radon' or 'HT'
 		file.write(f'dual: {dual}\n')
 		file.write('(time (s),memory (KB)): \n init cplx \n upper_star \n pre_processing \n transform \n total \n ext total \n\n')
 		file.close()
-
-	result = np.zeros((n_samples,len(sizes),len(spacings),5,2))
-	n_crit_pts = np.zeros((n_samples,len(sizes),len(spacings), 2))
-	total_ext = np.zeros((n_samples,len(sizes),len(spacings), 2))
+	m = max(len(sizes),len(spacings))
+	result = np.zeros((n_samples,m,5,2))
+	n_crit_pts = np.zeros((n_samples,m, 2))
+	total_ext = np.zeros((n_samples,m, 2))
 	for _ in range(n_samples):
-		for i, size in enumerate(sizes):
-			for j, spacing in enumerate(spacings):
-				print(f'sample = {_} | size = {size} | spacing = {spacing}')
-				cmd = '/usr/bin/time --output=' + path_to_savings + '-total-logs.txt -f "%U %M" python3 subprocess_mem.py ' +  str(size) + ' ' + str(spacing) + ' ' + str(n_dir) + ' ' + str(dual) + ' ' + str(dim) + ' ' + path_to_savings + ' ' + transform
-				output = subprocess.check_output(cmd, shell=True)
-				temp_res = [float(_.decode()) for _ in output.split()]
-				result[_,i,j] = np.array([(temp_res[2*_],temp_res[2*_+1]) for _ in range(4)]+[(sum(temp_res[2*_] for _ in range(4)), sum(temp_res[2*_+1] for _ in range(4)))])
-				n_crit_pts[_,i,j] = np.array([int(temp_res[-2]), int(temp_res[-1])])
-				with open(path_to_savings+'-logs.txt', 'a+') as file:
-					file.write(f'\n nbr critical points (cla,ord): {n_crit_pts[_,i,j]}\n')
-					file.write(f'result:\n{result[_,i,j]}\n')
-					file.close()
-				with open(path_to_savings+'-total-logs.txt', 'r') as file:
-					line = file.readline().split()
-					total_ext[_,i,j] = np.array([float(line[0]), int(line[1])])
+		for i in range(m):
+			size = sizes[min(len(sizes)-1,i)]
+			spacing = spacings[min(len(spacings)-1,i)]
+			print(f'sample = {_} | size = {size} | spacing = {spacing}')
+			cmd = '/usr/bin/time --output=' + path_to_savings + '-total-logs.txt -f "%U %M" python3 subprocess_mem.py ' +  str(size) + ' ' + str(spacing) + ' ' + str(n_dir) + ' ' + str(dual) + ' ' + str(dim) + ' ' + path_to_savings + ' ' + transform
+			output = subprocess.check_output(cmd, shell=True)
+			temp_res = [float(_.decode()) for _ in output.split()]
+			result[_,i] = np.array([(temp_res[2*_],temp_res[2*_+1]) for _ in range(4)]+[(sum(temp_res[2*_] for _ in range(4)), sum(temp_res[2*_+1] for _ in range(4)))])
+			n_crit_pts[_,i] = np.array([int(temp_res[-2]), int(temp_res[-1])])
+			with open(path_to_savings+'-logs.txt', 'a+') as file:
+				file.write(f'\n nbr critical points (cla,ord): {n_crit_pts[_,i]}\n')
+				file.write(f'result:\n{result[_,i]}\n')
+				file.close()
+			with open(path_to_savings+'-total-logs.txt', 'r') as file:
+				line = file.readline().split()
+				total_ext[_,i] = np.array([float(line[0]), int(line[1])])
 	np.savez(path_to_savings, result=result, n_crit_pts=n_crit_pts, total_ext=total_ext)
 	print('Results saved in:', path_to_savings)
 
