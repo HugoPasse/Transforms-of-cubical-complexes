c9fcfd0b6a0720fb35fbdc5aa5d03299a9d7f8a6
vadimlebovici <49746979+vadimlebovici@users.noreply.github.com>
2023-07-13 10:46:54 +0200

    debugged

--- fashion_mnist_dem_mem.py
+++ fashion_mnist_dem_mem.py
@@ -25,23 +25,49 @@ if not full == 0 : full = 1
 
 data = pd.read_csv('fashion_mnist/fashion-mnist_train.csv', skiprows=lambda x:x not in [0,index])
 
+time_cplx, time_preproc, time_transform, mem_cplx, mem_preproc, mem_transform = 0, 0, 0, 0, 0, 0
 
 if full:
 	img = (data.iloc[:,1:]).to_numpy().reshape((28,28))
 	values = np.unique(img)
+	for val_pix in values:
+		img = np.zeros_like(X)
+		img[X >  val_pix] = 1
+		img[X <= val_pix] = 0
+		# Construct complex
+		mtic = process.memory_full_info().uss
+		tic = time.perf_counter()
+		cplx = euler.CubicalComplex(img)
+		toc = time.perf_counter()
+		mtoc = process.memory_full_info().uss
+		time_cplx += toc-tic
+		mem_cplx += mtoc-mtic
+
+		# Preprocessing
+		mtic = process.memory_full_info().uss
+		tic = time.perf_counter()
+		seed = cplx.complexify()
+		toc = time.perf_counter()
+		mtoc = process.memory_full_info().uss
+		time_preproc += toc-tic
+		mem_preproc += mtoc-mtic
+
+		# Computing transform
+		value = 0
+		directions = np.random.rand(n_dir, 2)
+		mtic = process.memory_full_info().uss
+		tic = time.perf_counter()
+		value = seed.ECT(directions, T)
+		toc = time.perf_counter()
+		mtoc = process.memory_full_info().uss
+		time_transform += toc-tic
+		mem_transform += mtoc-mtic
 else:
 	X = (data.iloc[:,1:]).to_numpy().reshape((28,28))
 	values = np.unique(X)
 	med = np.quantile(values, 0.5)
 	img = np.zeros_like(X)
 	img[X>med] = 1
-
-time_cplx, time_preproc, time_transform, mem_cplx, mem_preproc, mem_transform = 0, 0, 0, 0, 0, 0
-
-for val_pix in values:
-	img = np.zeros_like(X)
-	img[X >  val_pix] = 1
-	img[X <= val_pix] = 0
 	# Construct complex
 	mtic = process.memory_full_info().uss
 	tic = time.perf_counter()
